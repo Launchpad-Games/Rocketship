f1c02644ea597a5703d31ba187e30bf292f00d40
Maxb0tbeep <maxcooley04@gmail.com>
2022-05-30 23:07:44 -0700

    v1.1.2

--- src/Events/messageCreate.js
+++ src/Events/messageCreate.js
@@ -6,7 +6,7 @@ module.exports = new Event("messageCreate", (client, message) => {
 	if (message.author.bot) return;
 
 	try{
-		if(message.mentions.members.first().id === '573248130389114880' || message.mentions.members.first().id === '423625542722387969'){
+		if(message.mentions.members.has('573248130389114880') || message.mentions.members.has('423625542722387969')){
 			if(!message.member.permissions.has('MENTION_EVERYONE')){
 				message.channel.send(`<@${message.author.id}> Please Don't Ping Developers! <a:petMax:956028370968264758> <a:petLuka:956028370959888404>`);
 				message.delete();
