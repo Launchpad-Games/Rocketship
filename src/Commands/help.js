2999af649b3a3a54d72bfaa2460f3415baee4613
Maxb0tbeep <maxcooley04@gmail.com>
2022-05-30 22:16:07 -0700

    v1.1

--- src/Commands/help.js
+++ src/Commands/help.js
@@ -9,12 +9,12 @@ module.exports = new Command({
     async run(message, args, client){
         const helpEmbed = new Discord.MessageEmbed()
             .setTitle("Help")
-            .setDescription("Learn how to use Rocketship! Prefix is >")
-            .setColor("#4e06dd")
+            .setDescription("Learn how to use Rocketship! Prefix is >\nCurrent: v1.1")
+            .setColor("#a914ff")
             .setThumbnail("https://cdn.discordapp.com/attachments/780618724952113162/981053549364527214/rocketship.png")
             .setTimestamp()
             .setURL("https://launchpad-games.com")
-
+            
             fs.readdirSync("./src/Commands")
 			    .filter(file => file.endsWith(".js"))
 			    .forEach(file => {
