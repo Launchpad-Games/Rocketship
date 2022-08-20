30ff24ad936beb335991c3e4a729f42a54b66a02
Maxb01beep <maxbotbeep@technologist.com>
2022-08-20 08:53:12 +0500

    jdfmgdfg

--- src/Events/messageCreate.js
+++ src/Events/messageCreate.js
@@ -1,6 +1,9 @@
 /** @format */
 
 const Event = require("../Structures/Event.js");
+const fs = require("fs");
+const path = require("path");
+const { MessageAttachment } = require("discord.js");
 
 module.exports = new Event("messageCreate", (client, message) => {
 	if (message.author.bot) return;
@@ -9,11 +12,18 @@ module.exports = new Event("messageCreate", (client, message) => {
 		if(message.mentions.members.has('573248130389114880') || message.mentions.members.has('423625542722387969')){
 			if(!message.member.permissions.has('MENTION_EVERYONE')){
 				message.channel.send(`<@${message.author.id}> Please Don't Ping Developers! <a:petMax:956028370968264758> <a:petLuka:956028370959888404>`);
-				message.delete();
+				// message.delete();
+				message.member.timeout(45 * 1000, "Pinged Dev")
 				return message.channel.send;
 			}
 		}
 	}catch (err) {}
+
+	const messageToCheck = message.content.toLowerCase();
+	result = messageToCheck.includes("grappleshot release date");
+	if(result){
+		message.reply("https://imgur.com/a/RcwGw4R");
+	}
 
 	if (!message.content.startsWith(client.prefix)) return;
 
