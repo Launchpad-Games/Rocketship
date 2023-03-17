b380d1c9ce95a6a170cdeb2a5bb102bff7828064
Maxb0tbeep <max_cooley@icloud.com>
2023-03-16 17:03:59 -0700

    sync with laptop

--- schemas/EconomySchema.js
+++ schemas/EconomySchema.js
@@ -3,9 +3,8 @@ const mongoose = require("mongoose");
 const EconomySchema = new mongoose.Schema({
     _id:{type:mongoose.SchemaTypes.String,required:true},
     username:{type:mongoose.SchemaTypes.String,required:true},
-    level:{type: mongoose.SchemaTypes.Number,required:true},
-    credits:{type: mongoose.SchemaTypes.Number,required:true},
-    xp:{type: mongoose.SchemaTypes.Number,required:true}
+    messages:{type: mongoose.SchemaTypes.Number,required:true},
+    credits:{type: mongoose.SchemaTypes.Number,required:true}
 });
 
 module.exports = mongoose.model("Economy", EconomySchema);
